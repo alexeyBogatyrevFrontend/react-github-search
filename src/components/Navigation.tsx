4224dee12e3c77fe9865bff38884cc7281e4a719
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2023-10-10 17:57:14 +0600

    done github search

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+const Navigation = () => {
+	return (
+		<nav className='flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white'>
+			<h3 className='font-bold'>Github Search</h3>
+
+			<span className='flex gap-5'>
+				<Link to='/'>Home</Link>
+				<Link to='/favourites'>Favourites</Link>
+			</span>
+		</nav>
+	)
+}
+
+export default Navigation
